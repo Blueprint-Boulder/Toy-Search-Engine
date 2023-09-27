8195380e52c6b387a461297d7f0e6888ccfc1999
Ishan Gohil <86436938+IshanGProjects@users.noreply.github.com>
2023-09-27 16:45:07 -0600

    db test

--- toyDb/buildToyDb.py
+++ toyDb/buildToyDb.py
@@ -5,24 +5,27 @@ import os
 # Load environment variables
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
+DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+DB_DATABASE = os.getenv("DB_DATABASE")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_DIALECT = os.getenv("DB_DIALECT")  # This variable is not used in the connection, but it's loaded here for completeness.
 
 def create_database():
     # Establish a connection to the MySQL server without specifying the database
     connection = mysql.connector.connect(
         host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASSWORD
+        user=DB_USERNAME,
+        password=DB_PASSWORD,
+        port=DB_PORT
     )
     cursor = connection.cursor()
 
     try:
         # Create a new database
-        cursor.execute(f"CREATE DATABASE {DB_NAME}")
-        print(f"Database {DB_NAME} created successfully!")
+        cursor.execute(f"CREATE DATABASE {DB_DATABASE}")
+        print(f"Database {DB_DATABASE} created successfully!")
     except mysql.connector.Error as err:
         print(f"Error: {err}")
     finally:
