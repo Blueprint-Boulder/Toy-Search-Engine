176a16de9bc3af45ebcfaff10373a654898d6ea9
Ishan Gohil <86436938+IshanGProjects@users.noreply.github.com>
2023-09-27 17:52:34 -0600

    added code to implement the creation of the tables with its corresponding foreign keys

--- toyDb/buildToyDb.py
+++ toyDb/buildToyDb.py
@@ -23,9 +23,50 @@ def create_database():
     cursor = connection.cursor()
 
     try:
+        # Check if the database exists
+        cursor.execute(f"SHOW DATABASES LIKE '{DB_DATABASE}'")
+        result = cursor.fetchone()
+
+        if result:
+            # If the database exists, drop it
+            cursor.execute(f"DROP DATABASE {DB_DATABASE}")
+            print(f"Database {DB_DATABASE} existed and was dropped.")
+
         # Create a new database
         cursor.execute(f"CREATE DATABASE {DB_DATABASE}")
         print(f"Database {DB_DATABASE} created successfully!")
+
+        # Use the new database to create tables
+        cursor.execute(f"USE {DB_DATABASE}")
+
+        # Create Families table
+        cursor.execute("""
+        CREATE TABLE Families (
+            family_id INT AUTO_INCREMENT PRIMARY KEY,
+            family_name VARCHAR(255) NOT NULL,
+            subfamily_name VARCHAR(255)
+        );
+        """)
+
+        # Create Butterflies table
+        cursor.execute("""
+        CREATE TABLE Butterflies (
+            butterfly_id INT AUTO_INCREMENT PRIMARY KEY,
+            family_id INT,
+            species_name VARCHAR(255) NOT NULL,
+            scientific_name VARCHAR(255) NOT NULL,
+            appearance TEXT,
+            wingspan VARCHAR(255),
+            habitat TEXT,
+            flight_times TEXT,
+            larval_foodplant TEXT,
+            additional_info TEXT,
+            FOREIGN KEY (family_id) REFERENCES Families(family_id)
+        );
+        """)
+
+        print("Tables created successfully!")
+
     except mysql.connector.Error as err:
         print(f"Error: {err}")
     finally:
